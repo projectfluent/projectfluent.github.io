697b977feb33c897bd8ed2351a848a6bdb843405
Staś Małolepszy <stas@mozilla.com>
2017-03-09 10:30:04 +0100

    Update to Fluent Syntax 0.3

--- src/demo.js
+++ src/demo.js
@@ -5,21 +5,23 @@ import {
     annotation_display, parse_translations, create_context, format_messages
 } from './fluent';
 
-const translations = `# Try editing the translations here!
+const translations = `// Try editing the translations here!
 
 hello-world = Hello, world!
 
-shared-photos = { $user_name } { $photo_count ->
-    [0] hasn't added any photos yet
-    [one] added a new photo
-   *[other] added { $photo_count } new photos
-}.
+shared-photos =
+    { $user_name } { $photo_count ->
+        [0] hasn't added any photos yet
+        [one] added a new photo
+       *[other] added { $photo_count } new photos
+    }.
 
-liked-comment = { $user_name } liked your comment on { $user_gender ->
-    [male] his
-    [female] her
-   *[other] their
-} post.
+liked-comment =
+    { $user_name } liked your comment on { $user_gender ->
+        [male] his
+        [female] her
+       *[other] their
+    } post.
 `;
 
 function Message(props) {
