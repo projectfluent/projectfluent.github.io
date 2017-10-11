5a4468530826640513de818db53312d4e388a879
Staś Małolepszy <stas@mozilla.com>
2017-10-11 16:57:40 +0200

    Move Examples into index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Demo from './demo';
-import Examples from './examples';
+import Example from './example';
+import { TextInput, RadioInput, RangeInput } from "./controls";
+
+const example0 = {
+    translations: `hello-user = Hello, { $user }!
+
+unread-emails =
+    You have { $emails_count ->
+        [0] no unread emails
+        [one] one unread email
+       *[other] { $emails_count } unread emails
+    }.`,
+    externals: {
+        name: "Anne",
+        emails_count: 3,
+    }
+};
+
+function Example0() {
+    return (
+        <Example {...example0}>
+            <TextInput name="user" />
+            <RangeInput name="emails_count" min="0" max="9" step="1" />
+        </Example>
+    );
+}
 
 ReactDOM.render(
-   <Demo />,
+   <Example0 />,
    document.getElementById('demo-app')
 );
+
+const example1 = {
+    translations: `shared-photos =
+    { $name } { $photo_count ->
+        [0] hasn't added any photos yet
+        [one] added a new photo
+       *[other] added { $photo_count } new photos
+    }.`,
+    externals: {
+        name: "Anne",
+        photo_count: 3,
+    }
+};
+
+const example2 = {
+    translations: `liked-comment =
+    { $name } liked your comment on { $gender ->
+        [male] his
+        [female] her
+       *[other] their
+    } post.`,
+    externals: {
+        name: "John",
+        gender: "male",
+    }
+};
+
+function Examples() {
+    return [
+        <Example {...example1} chevron="right">
+            <TextInput name="name" />
+            <RangeInput name="photo_count" min="0" max="9" step="1" />
+        </Example>,
+        <Example {...example2} chevron="right">
+            <TextInput name="name" />
+            <RadioInput name="gender" options={["male", "female", "unknown"]} />
+        </Example>
+    ];
+}
 
 ReactDOM.render(
    <Examples />,
