1fd7bc86ed19df46e049bdf901c7b925d7c5c358
Staś Małolepszy <stas@mozilla.com>
2019-06-28 16:34:45 +0200

    Use @fluent/dedent in example0

--- src/index.js
+++ src/index.js
@@ -9,20 +9,21 @@ import { TextInput, RadioInput, RangeInput } from "./controls";
 
 const example0 = {
     locale: "en-US",
-    translations: `\
-# Simple things are simple.
-hello-user = Hello, {$userName}!
+    translations: ftl`
+        # Simple things are simple.
+        hello-user = Hello, {$userName}!
 
-# Complex things are possible.
-shared-photos =
-    {$userName} {$photoCount ->
-        [one] added a new photo
-       *[other] added {$photoCount} new photos
-    } to {$userGender ->
-        [male] his stream
-        [female] her stream
-       *[other] their stream
-    }.`,
+        # Complex things are possible.
+        shared-photos =
+            {$userName} {$photoCount ->
+                [one] added a new photo
+            *[other] added {$photoCount} new photos
+            } to {$userGender ->
+                [male] his stream
+                [female] her stream
+            *[other] their stream
+            }.
+        `,
     externals: {
         userName: "Anne",
         userGender: "female",
