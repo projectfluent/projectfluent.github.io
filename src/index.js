b7c77df76e1dc68603a8651ff1d7e4fb2bd8d22a
Jian Zhang <dsonet@msn.com>
2022-03-14 15:07:28 -0700

    fix a singular form error in example

--- src/index.js
+++ src/index.js
@@ -57,9 +57,10 @@ const example1 = {
            *[other] Close {$tabCount} tabs
         }
         tabs-close-warning =
-            You are about to close {$tabCount} tabs.
+            You are about to close {$tabCount ->
+                [one] {$tabCount} tab
+               *[other] {$tabCount} tabs}.
             Are you sure you want to continue?
-
         ## Syncing
 
         -sync-brand-name = Firefox Account
